4307039441458c26f3b01dcd1290398a7605b546
Leah <amelia_rawls@ccpsnet.net>
2022-03-26 05:47:14 -0400

    initial layout

---
+++ assets/js/script.js
@@ -0,0 +1,55 @@
+// store the answer choices
+var choices = [];
+
+var myQuestions = [
+   {
+     question: 'What is the block of javaScript code that is executed when "called"?',
+     answers: {
+       a: "event",
+       b: "function",
+       c: "string",
+       d: "object"
+     },
+     correctAnswer: "b"
+   },
+   {
+     question: "Which one is a way that you CANNOT delare a variable?",
+     answers: {
+       a: "var",
+       b: "let",
+       c: "declare",
+       d: "const"
+     },
+     correctAnswer: "c"
+   },
+   {
+     question: "What digit represents the first element in an array?",
+     answers: {
+       a: "0",
+       b: "1",
+       c: "-1",
+       d: "i"
+     },
+     correctAnswer: "a"
+   },
+   {
+     question: "What can be used to perform different actions based on different conditions?",
+     answers: {
+        a: "if",
+        b: "then",
+        c: "else",
+        d: "switch"
+     },
+     correctAnswer: "d"
+   },
+   {
+     question: "What statement lets you get out of a loop?",
+     answers: {
+        a: "end",
+        b: "break",
+        c: "jump",
+        d: "let"
+     },
+     correctAnswer: "b"
+   }
+ ];
