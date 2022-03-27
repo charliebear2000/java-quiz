d32c2bc4f5a84707832ce1df192174c27b0ee04b
Leah <amelia_rawls@ccpsnet.net>
2022-03-27 08:28:51 -0400

    added timer

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,32 @@
 // store the answer choices
+var timerEl = document.getElementById('countdown');
 var choices = [];
+
+
+// Timer that counts down from 20
+function countdown() {
+  var timeLeft = 20;
+
+  // Use the `setInterval()` method to call a function to be executed every 1000 milliseconds
+  var timeInterval = setInterval(function () {
+    // As long as the `timeLeft` is greater than 1
+    if (timeLeft > 1) {
+      // Set the `textContent` of `timerEl` to show the remaining seconds
+      timerEl.textContent = timeLeft + ' seconds remaining';
+      // Decrement `timeLeft` by 1
+      timeLeft--;
+    } else if (timeLeft === 1) {
+      // When `timeLeft` is equal to 1, rename to 'second' instead of 'seconds'
+      timerEl.textContent = timeLeft + ' second remaining';
+      timeLeft--;
+    } else {
+      // Once `timeLeft` gets to 0, set `timerEl` to an empty string
+      timerEl.textContent = 'Time is up!';
+      // Use `clearInterval()` to stop the timer
+      clearInterval(timeInterval);
+    }
+  }, 1000);
+}
 
 var myQuestions = [
    {
